2ee05647b610bffc96c5ae1464269a3c0bf8f91c
Cristian Guerrero <crguereros@unal.edu.co>
2025-04-23 15:42:29 -0500

    Prueba

--- funciones.py
+++ funciones.py
@@ -1,18 +1,22 @@
 #def f(x):
     #  return x * x
 
-import math
+#import math
 
 #print(f(3))
 
-def area_circulo(r):
-    result = math.pi 
+#def area_circulo(r):
+    #result = math.pi 
 
-def area_rectangulo(l,a):
-    area = l * a
-    return area
+#def area_rectangulo(l,a):
+    #area = l * a
+    #return area
 
-largo = float(input("ingrese el largo del rectangulo "))
-ancho = float(input("ingrese el ancho del rectangulo "))
+#largo = float(input("ingrese el largo del rectangulo "))
+a#ncho = float(input("ingrese el ancho del rectangulo "))
 
-print("El area del rectangulo corresponde a ", area_rectangulo(largo, ancho))
+#print("El area del rectangulo corresponde a ", area_rectangulo(largo, ancho))
+
+#Ejercicio coulomb
+
+KAPPA = 
