c4eb01dc82c340adaad94c76e6962b314e00f590
Cristian Guerrero <crguereros@unal.edu.co>
2025-04-23 15:20:24 -0500

    first commit

---
+++ funciones.py
@@ -0,0 +1,18 @@
+#def f(x):
+    #  return x * x
+
+import math
+
+#print(f(3))
+
+def area_circulo(r):
+    result = math.pi 
+
+def area_rectangulo(l,a):
+    area = l * a
+    return area
+
+largo = float(input("ingrese el largo del rectangulo "))
+ancho = float(input("ingrese el ancho del rectangulo "))
+
+print("El area del rectangulo corresponde a ", area_rectangulo(largo, ancho))
